f3deaebd3a2b67286e746356ce64cfff89809216
Marcus Smith <marcus@axoncollective.com>
2021-01-17 16:11:33 -0500

    Initial setup, added in Tailwind (looks to be working sofar

---
+++ vue-feedback/src/App.vue
@@ -0,0 +1,20 @@
+<template>
+  <div id="app">
+    <VueFeedbackPopup/>
+  </div>
+</template>
+
+<script>
+import VueFeedbackPopup from './components/VueFeedbackPopup.vue'
+
+export default {
+  name: 'app',
+  components: {
+    VueFeedbackPopup
+  }
+}
+</script>
+
+<style>
+
+</style>
---
+++ vue-feedback/src/components/VueFeedbackPopup.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="fixed flex flex-column w-screen h-screen justify-center items-center bg-black">
+    <h1 >Start</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "VueFeedbackPopup",
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style lang="postcss" scoped>
+
+h1 {
+  @apply text-white text-3xl;
+}
+</style>
